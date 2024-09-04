3094e711adb490de1e16382fac4afdb271616411
Aryan Malik <aryanhunny98@gmail.com>
2024-09-05 01:05:53 +0530

    Created the Navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Navbar from '../components/Navbar';
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-    
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
