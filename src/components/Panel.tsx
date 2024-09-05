5820cb4ad98996dcc98046a2d54bc907f134e0ab
Aryan Malik <aryanhunny98@gmail.com>
2024-09-05 10:02:39 +0530

    Moved components css to component directory

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styles from '../styles/Panel.module.css';
+import styles from './styles/Panel.module.css';
 
 const Panel = () => {
   const [isDeposit, setIsDeposit] = useState(true);
